a39ced40f7c93b5a627f3dbf9ca584e715b9a5a9
Olexandr Tkachuk <temishena@gmail.com>
2022-12-14 19:33:29 +0200

    simpleLightBox add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,18 +1,28 @@
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryContainerRef = document.querySelector('.gallery');
 const markup = createMarkup(galleryItems);
 
+galleryContainerRef.insertAdjacentHTML('beforeend', markup);
+console.log(galleryContainerRef);
+
 function createMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__link" href="${original}">
+      return `<a class="gallery__item" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}">
               </a>`;
     })
     .join('');
 }
 
-galleryContainerRef.insertAdjacentHTML('beforeend', markup);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsPosition: 'bottom',
+  captionsDelay: 250,
+});
 
-console.log(galleryContainerRef);
+// lightbox.on('show.simplelightbox', function () {});
