1dcf2c67538a1c9e1f280c664dc2fbe2f108f61b
Olexandr Tkachuk <temishena@gmail.com>
2022-12-14 22:50:45 +0200

    vimeo player initialization

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const storageKey = 'videoplayer-current-time';
+const currentStorageValue = localStorage.getItem(storageKey);
+console.log(currentStorageValue);
+
+player.on('timeupdate', throttle(onPlayer, 1000));
+
+function onPlayer(data) {}
